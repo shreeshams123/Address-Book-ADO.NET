504a44cfae7df783b61593e134f01497fc8b92ae
shreeshams123 <[shreesha2002ms@gmail.com]>
2024-10-17 10:32:01 +0530

    ADDED Main class

---
+++ Address-Book-ADO.NET/AddressBookMain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Address_Book_ADO.NET
+{
+    internal class AddressBookMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the address book project");
+        }
+    }
+}
