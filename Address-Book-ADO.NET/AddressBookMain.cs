b27d4dda725979ef583a8717215a8c61e8fe524b
shreeshams123 <[shreesha2002ms@gmail.com]>
2024-10-17 12:54:35 +0530

    Added functionality to add contacts

--- Address-Book-ADO.NET/AddressBookMain.cs
+++ Address-Book-ADO.NET/AddressBookMain.cs
@@ -8,7 +8,38 @@ namespace Address_Book_ADO.NET
         {
             Console.WriteLine("Welcome to the address book project");
             ContactRepo contactRepo = new ContactRepo();
-            contactRepo.CreateContactTable();
+            //contactRepo.CreateContactTable();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("MENU");
+                Console.WriteLine("1.Add contact");
+                Console.WriteLine("Enter your choice");
+                int choice=Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1: Console.WriteLine("Enter FirstName");
+                        string firstname= Console.ReadLine();
+                        Console.WriteLine("Enter Lastname");
+                        string lastname= Console.ReadLine();
+                        Console.WriteLine("Enter Address");
+                        string address= Console.ReadLine();
+                        Console.WriteLine("Enter City");
+                        string city= Console.ReadLine();
+                        Console.WriteLine("Enter state");
+                        string state= Console.ReadLine();
+                        Console.WriteLine("Enter zip");
+                        int zip=Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter phone number");
+                        long phone=Convert.ToInt64(Console.ReadLine());
+                        Console.WriteLine("Enter email id");
+                        string email= Console.ReadLine();
+                        contactRepo.AddContacts(firstname, lastname, address, city, state, zip, phone, email);
+                        break;
+
+                }
+                
+            }
         }
     }
 }
--- Address-Book-ADO.NET/ContactRepo.cs
+++ Address-Book-ADO.NET/ContactRepo.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Data.Common;
 using System.Data.SqlClient;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
 namespace Address_Book_ADO.NET
 {
     public class ContactRepo
@@ -18,7 +19,7 @@ namespace Address_Book_ADO.NET
         }
         public void CreateContactTable()
         {
-            string query =@"Create Table Contacts(FirstName Varchar(20) primary key, LastName Varchar(20),Address Varchar(50),City Varchar(10),State Varchar(20),Zip INT,Phone BIGINT,Email Varchar(20))";
+            string query =@"Create Table Contacts(FirstName Varchar(20) primary key, LastName Varchar(20),Address Varchar(50),City Varchar(10),State Varchar(20),Zip INT,Phone BIGINT,Email Varchar(50))";
             using (SqlConnection con = new SqlConnection(connectionstring)) {
                 con.Open();
                 using (SqlCommand cmd = new SqlCommand(query, con))
@@ -29,6 +30,28 @@ namespace Address_Book_ADO.NET
              };
         
 
+        }
+        public void AddContacts(string firstname,string lastname,string address,string city,string state,int zip,long phone,string email)
+        {
+            string query= "INSERT INTO Contacts (FirstName, LastName, Address, City, State, Zip, Phone, Email) VALUES (@FirstName, @LastName, @Address, @City, @State, @Zip, @Phone, @Email)";
+            using (SqlConnection con = new SqlConnection(connectionstring))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    
+                    cmd.Parameters.AddWithValue("@FirstName",firstname);
+                    cmd.Parameters.AddWithValue("@LastName", lastname);
+                    cmd.Parameters.AddWithValue("@Address",address);
+                    cmd.Parameters.AddWithValue("@City",city);
+                    cmd.Parameters.AddWithValue("@State",state);
+                    cmd.Parameters.AddWithValue("@Zip",zip);
+                    cmd.Parameters.AddWithValue("@Phone",phone);
+                    cmd.Parameters.AddWithValue("@Email",email);
+                    cmd.ExecuteNonQuery();
+                    Console.WriteLine("Contact added successfully");
+                }
+            }
         }
     }
 }
