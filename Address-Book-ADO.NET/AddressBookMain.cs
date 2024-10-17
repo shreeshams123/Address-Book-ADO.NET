ccf08dd5709e774b7f8b54ce0f7e36208f6ecc75
shreeshams123 <[shreesha2002ms@gmail.com]>
2024-10-17 12:18:01 +0530

    Created Contacts Table

--- Address-Book-ADO.NET/AddressBookMain.cs
+++ Address-Book-ADO.NET/AddressBookMain.cs
@@ -7,6 +7,8 @@ namespace Address_Book_ADO.NET
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the address book project");
+            ContactRepo contactRepo = new ContactRepo();
+            contactRepo.CreateContactTable();
         }
     }
 }
---
+++ Address-Book-ADO.NET/ContactRepo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Data.Common;
+using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
+namespace Address_Book_ADO.NET
+{
+    public class ContactRepo
+    {
+        private string connectionstring;
+        public ContactRepo() {
+            var config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true,reloadOnChange: true).Build();
+            connectionstring = config.GetConnectionString("DefaultConnection");
+        }
+        public void CreateContactTable()
+        {
+            string query =@"Create Table Contacts(FirstName Varchar(20) primary key, LastName Varchar(20),Address Varchar(50),City Varchar(10),State Varchar(20),Zip INT,Phone BIGINT,Email Varchar(20))";
+            using (SqlConnection con = new SqlConnection(connectionstring)) {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.ExecuteNonQuery();
+                    Console.WriteLine("Table Created Successfully");
+                };
+             };
+        
+
+        }
+    }
+}
